55560436986b214afc6531cc2e9bc57f89c48ae6
Esvinded <118370905+Esvinded@users.noreply.github.com>
2024-05-13 00:59:54 +0700

    Update VIP.cpp

--- VIP.cpp
+++ VIP.cpp
@@ -1,9 +1,20 @@
 #include "VIP.h"
 
-VIP::VIP(const string& phoneNumber, const string& name, const string& password, double initialSpent)
-: Guest(phoneNumber, name, password), rank(0) {
-    moneySpent = initialSpent;
-    updateRank();
+VIP::VIP() {}
+VIP::VIP(string _type, string _phoneNumber, string _name, string _password, double _moneySpent, vector<Data> _bookingList, string _rank)
+: Guest(_type, _phoneNumber, _name, _password, _moneySpent, _bookingList) {
+    rank = _rank;
+}
+VIP::~VIP() {}
+
+string VIP::getRank() const
+{
+	return rank;
+}
+
+void VIP::setRank(string _rank)
+{
+	rank = _rank;
 }
 
 void VIP::billing() {
@@ -11,5 +22,18 @@ void VIP::billing() {
 }
 
 void VIP::updateRank() {
-    // Update rank based on money spent
+	if (moneySpent >= 200 || moneySpent <500)
+	{
+		rank = "Silver";
+	}
+	else
+	{
+		if (moneySpent >= 500 || moneySpent < 1000)
+		{
+			rank = "Gold";
+		}
+		else {
+			rank = "Diamond";
+		}
+	}
 }
