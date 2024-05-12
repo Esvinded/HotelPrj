b75137c7da8753c1c21846b8bbdde39a098b6923
Esvinded <118370905+Esvinded@users.noreply.github.com>
2024-05-12 23:52:48 +0700

    Update Guest.cpp

--- Guest.cpp
+++ Guest.cpp
@@ -33,7 +33,24 @@ void Guest::booking(int _room, string _checkInDate, string _checkOutDate) {
 }
 
 void Guest::canceling() {
-    bookingList.clear();
+    int choice;
+    if (bookingList.size() == 0) {
+        cout << "Booking List is empty" << endl;
+    }
+    else {
+        cout << "Choose a booking to cancel" << endl;
+        for (int i = 0; i < bookingList.size(); i++)
+        {
+            cout << i << ". " << bookingList[i].room << endl;
+        }
+        cin >> choice;
+        if (choice < 0 || choice >= bookingList.size()) {
+            cout << "Invalid choice" << endl;
+        } 
+        else {
+            bookingList.erase(bookingList.begin() + choice);
+        }
+    }
 }
 
 void Guest::billing() {
